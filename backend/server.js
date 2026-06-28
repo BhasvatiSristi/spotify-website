898f7ec8b80d905391abe7459d8a795b5e282582
Bhasvati Sristi <bhasvati2106@gmail.com>
2026-06-28 18:50:16 +0530

    songs part

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 import express from "express"
 import cors from "cors"
 import "dotenv/config"
+import songRouter from "./src/routes/songRoute.js"
+import connectDb from "./src/config/mongodb.js"
+import connectCloudinary from "./src/config/cloudinary.js"
 
 
 //app config
 const app = express()
 const port = process.env.PORT || 4000
+connectDb();
+connectCloudinary();
 
 //middleware
 app.use(express.json())
 app.use(cors())
 
+//initializing routes
+app.use("/api/song",songRouter)
 
 //routes
 app.get("/",(req,res)=>{
